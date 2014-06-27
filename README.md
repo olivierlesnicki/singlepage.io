18faf376503a4dc5de78c8368469bbe6a672fca5
Olivier Lesnicki <olivier@MacBook-Pro-de-Olivier.local>
2014-06-27 13:58:49 +0200

    Arranged folder

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ singlepage.io
 
 [![Build Status](https://travis-ci.org/olivierlesnicki/singlepage.io.svg?branch=master)](https://travis-ci.org/olivierlesnicki/singlepage.io)
 
-A single page application hosting service powered by Node.js.
+A web hosting service for single page application.
--- index.js
+++ index.js
@@ -1,12 +1,54 @@
 var express = require('express');
-var app = express();
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
 var router = express.Router();
 var domainRouter = require('./lib/router/domain');
 
+var app = express();
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', router);
 app.use('/domain', domainRouter);
 
-app.get('/', function(req, res){
-  res.send('hello world');
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.listen(process.env.PORT);
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
